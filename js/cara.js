a62faae204f9349e8d1041b12d5e8733d18a1458
unknown <you@example.com>
2022-11-08 23:25:13 -0500

    final

--- js/cara.js
+++ js/cara.js
@@ -1,38 +1,91 @@
-const lanzacara=document.getElementById('cara')
+let coin = document.querySelector(".coin");
+let flipBtn = document.querySelector("#cara");
+let flipBtn_2 = document.querySelector("#sello");
+let resetBtn = document.querySelector("#reset-button");
+let cont=0;
+let acu=0;
+let total=0;
+let apu= document.querySelector("#apos");
+let opc = document.querySelector("#cara");
+let opcion = document.querySelector("#sello");
+flipBtn.addEventListener("click", () => {
+    let i = Math.floor(Math.random() * 2);
+    cont++
+    coin.style.animation = "none";
+    let opc1=parseInt(opc.value)
+    let apu1=parseInt(apu.value)
+    total=total+apu1;
+     
+    if(i){
+        coin.style.animation = "spin-heads 3s forwards";
+        setTimeout(function(){
+            if(i===opc1)
+        {                   
+        acu=acu=(parseInt(acu+(apu1*2)));
+        swal("Ha ganado", "Ha caido cara, su acumulado es de "+acu);                 
+        }         
+        }, 4000);
+        heads++;
+    }
+    else{
+        coin.style.animation = "spin-tails 3s forwards";
+        setTimeout(function(){
+            if(i!==opc1)
+            {              
+                acu=(parseInt(acu-apu1));                                           
+                swal("Ha Perdido", "Ha caido sello, su acumulado es de "+acu);                            
+            }         
+        }, 4000);
+        tails++;
+    }
+    setTimeout(updateStats, 3000);
+    disableButton();
+});
 
-lanzacara.addEventListener('click',()=>
-{    
-    apos=document.getElementById('apos').value
-    apos=parseInt(apos) 
-    cara=document.getElementById('cara').value
+flipBtn_2.addEventListener("click", () => {
+    let i = Math.floor(Math.random() * 2);
+    cont++
+    coin.style.animation = "none";
+    let opcion1=parseInt(opcion.value)
+    let apuesta1=parseInt(apu.value)
+    total=total+apuesta1;
     
-    let total=0;
-    let acu=0;
-    let cont=0;
-    opc=confirm('Desea comenzar a jugar?');
+     
+    if(i){
+        coin.style.animation = "spin-tails 3s forwards";
+        setTimeout(function(){
+            if(i!==opcion1)
+        {                                                         
+            acu=(parseInt(acu+(apuesta1*2)));
+            swal("Ha ganado" , "Ha caido sello, su acumulado es de "+acu);                   
+        }         
+        }, 4000);
+        heads++;
+    }
+    else{
+        coin.style.animation = "spin-heads 3s forwards";
+        setTimeout(function(){
+            if(i!==opcion1)
+            {                                                         
+                acu=(parseInt(acu-apuesta1));                                           
+                swal("Ha Perdido", "Ha caido cara, su acumulado es de "+acu);                 
+            }         
+        }, 4000);
+        tails++;
+    }
+    setTimeout(updateStats, 3000);
+    disableButton();
+});
+
+
+function disableButton(){
+    flipBtn.disabled = true;
+    setTimeout(function(){
+        flipBtn.disabled = false;
+    },3000);
+}
+resetBtn.addEventListener("click",() => {    
     
-    while(opc==true){
-    alert('¡¡¡¡¡Se hace el lanzamiento de la moneda!!!!')
-    random=Math.floor(Math.random() *2) +1;
-    random=parseInt(random)
-    if(cara==random)
-    {        
-        alert("A caido cara")    
-        alert(`Usted ha ganado`)        
-        acu=(parseInt(acu+(apos*2)));
-        alert(acu)        
-    }  
-    else
-    {
-        alert("A caido sello")    
-        alert(`Usted ha perdio`)        
-        acu=(parseInt(acu-apos));
-        alert(acu)        
-    }
-    cont=cont+1;
-    opc=confirm('Desea seguir jugando');
-    }
-    total=total+acu;
-    alert(`Su dinero total fue de ${total}`)
-    alert(`La cantidad de veces que usted jugo fue de ${cont}`)
-})
+    swal("Usted jugo "+cont+" veces y su dinero total fue "+total)
+});
+
---
+++ js/sweetAlert.js
@@ -0,0 +1,9 @@
+Swal.fire({
+    title: "Bienvenido",
+    text: "hola"
+});
+Swal.fire({
+    
+    title: "Bienve",
+    text: "adios"
+});
